e2a0c34d339a93e18cc565f10f3f7887f1db4ba0
Bezenger Tomy <tbezenger@excilys.com>
2018-02-22 10:08:51 +0100

    imports

---
+++ src/test/java/com/excilys/formation/tbezenger/ComputerDAOTest.java
@@ -0,0 +1,29 @@
+package com.excilys.formation.tbezenger;
+
+import org.mockito.Mock;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+import com.excilys.formation.tbezenger.DAO.ComputerManager;
+import com.excilys.formation.tbezenger.Exceptions.DatabaseException;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class ComputerDAOTest {
+
+    /*@Mock
+    ComputerManager cM;*/
+
+    /**
+     * @throws DatabaseException
+     */
+    /*@Test(expected = DatabaseException.class)
+    public void testApp() throws DatabaseException {
+        cM = mock(ComputerManager.class);
+        //when(cM.findById(1).orElse(new Computer())).thenThrow(SQLException.class);
+        cM.findById(1);
+    }*/
+}
