8e257b2dfbb1e31712638ec99e3ccc03cac1d6ad
Bezenger Tomy <tbezenger@excilys.com>
2018-02-23 11:07:18 +0100

    create DTO

---
+++ src/main/java/com/excilys/formation/tbezenger/DTO/CompanyDTO.java
@@ -0,0 +1,18 @@
+package com.excilys.formation.tbezenger.DTO;
+
+public class CompanyDTO {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/com/excilys/formation/tbezenger/DTO/ComputerDTO.java
@@ -0,0 +1,42 @@
+package com.excilys.formation.tbezenger.DTO;
+
+import java.sql.Date;
+
+public class ComputerDTO {
+	private int id;
+	private String name;
+	private Date introduced;
+	private Date discontinued;
+	private CompanyDTO company;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(Date introduced) {
+		this.introduced = introduced;
+	}
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(Date discontinued) {
+		this.discontinued = discontinued;
+	}
+	public CompanyDTO getCompany() {
+		return company;
+	}
+	public void setCompany(CompanyDTO company) {
+		this.company = company;
+	}
+}
