126a18c02e0d28dc1153d43ce4a3f137138f981b
Bezenger Tomy <tbezenger@excilys.com>
2018-03-21 12:09:53 +0100

    [REFACTO](webapp) merging controllers for add and edit computers

--- src/main/java/com/excilys/formation/tbezenger/servlets/ComputerSpringController.java
+++ src/main/java/com/excilys/formation/tbezenger/servlets/ComputerSpringController.java
@@ -1,5 +1,8 @@
 package com.excilys.formation.tbezenger.servlets;
 
+import static com.excilys.formation.tbezenger.Strings.COMPANIES;
+import static com.excilys.formation.tbezenger.Strings.COMPUTER;
+
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 import com.excilys.formation.tbezenger.DTO.ComputerDTO;
 import com.excilys.formation.tbezenger.DTO.Mapper;
 import com.excilys.formation.tbezenger.exceptions.DAO.DatabaseException;
@@ -23,21 +25,56 @@ import com.excilys.formation.tbezenger.model.Computer;
 import com.excilys.formation.tbezenger.services.CompanyService;
 import com.excilys.formation.tbezenger.services.ComputerService;
 
-import static com.excilys.formation.tbezenger.Strings.COMPUTER;
-import static com.excilys.formation.tbezenger.Strings.COMPANIES;
-
 @Controller
-public class EditComputerSpringController {
-	@Autowired
-	private CompanyService companyService;
+public class ComputerSpringController {
 	@Autowired
 	private ComputerService computerService;
+	@Autowired
+	private CompanyService companyService;
 	@Autowired
 	private ComputerDTOValidator computerValidator;
 
 	@InitBinder
 	private void initBinder(WebDataBinder binder) {
 		binder.setValidator(computerValidator);
+	}
+
+	@GetMapping("addComputer")
+	public String getAddComputerPage(ModelMap model) {
+		try {
+			model.addAttribute(COMPANIES, Mapper.toCompanyDTOList(companyService.getAll()));
+			model.addAttribute("addForm", new ComputerDTO());
+			return ("addComputer");
+		} catch (DatabaseException e) {
+			return "500";
+		}
+	}
+
+
+	@PostMapping("addComputer")
+	public String addComputer(@ModelAttribute("addForm") @Validated(ComputerDTO.class) ComputerDTO computerDTO, BindingResult bindingResult, ModelMap model) {
+		try {
+			if (!bindingResult.hasErrors()) {
+				if (createComputer(computerDTO.getName(), computerDTO.getIntroduced(), computerDTO.getDiscontinued(), Integer.toString(computerDTO.getCompany().getId()))) {
+					return "redirect:dashboard";
+				}
+			}
+			model.addAttribute(COMPANIES, Mapper.toCompanyDTOList(companyService.getAll()));
+			return "addComputer";
+		} catch (DatabaseException e) {
+			return "500";
+		}
+	}
+
+
+	public boolean createComputer(String name, String introduced, String discontinued, String companyId) throws DatabaseException {
+		ComputerDTO computerDTO = new ComputerDTO();
+		computerDTO.setName(name);
+		computerDTO.setDiscontinued(discontinued);
+		computerDTO.setIntroduced(introduced);
+		computerDTO.setCompany(Mapper.toDTO(companyService.get(Integer.parseInt(companyId)).orElse(new Company())));
+		return computerService.create(Mapper.toComputer(computerDTO));
+
 	}
 
 	@GetMapping("editComputer")
